b6e95ebce8e44a974a79335f8764058a9c67ade9
Vennela Reddy Karaddi <57874359+vennelakaraddi@users.noreply.github.com>
2021-09-11 21:51:31 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import { ReactComponent as Logo } from "./logo.svg";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Logo />
+    </div>
+  );
+}
